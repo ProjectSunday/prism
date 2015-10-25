a57249f92a339efd2d841bca63e2349681ebc41c
hai5nguy <hai5nguy@gmail.com>
2015-10-25 15:01:49 -0400

    dist gone

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,2 +1,15 @@
-var gulp = require('gulp');
+var gulp 		= require('gulp');
+var browserify 	= require('gulp-browserify');
 
+gulp.task('default', ['html']);
+
+gulp.task('html', function () {
+	gulp.src('./src/front/index.html')
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('scripts', function () {
+	gulp.src('./src/front/**/*.js')
+		.pipe(browserify())
+		.pipe(gulp.dest('./dist'))
+});
