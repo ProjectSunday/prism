d9e3a039c84ee90c96bed539f4903226a3a5f737
hai5nguy <hai5nguy@gmail.com>
2015-10-25 15:21:54 -0400

    working on first react components

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,20 @@
 var gulp 		= require('gulp');
-var browserify 	= require('gulp-browserify');
+var browserify 	= require('browserify');
+var reactify	= require('reactify');
+var source		= require('vinyl-source-stream');
 
-gulp.task('default', ['html']);
+gulp.task('default', ['html', 'js']);
 
 gulp.task('html', function () {
 	gulp.src('./src/front/index.html')
 		.pipe(gulp.dest('./dist'));
 });
 
-gulp.task('scripts', function () {
-	gulp.src('./src/front/**/*.js')
-		.pipe(browserify())
-		.pipe(gulp.dest('./dist'))
+gulp.task('js', function () {
+	browserify('./src/front/index.js')
+		.transform(reactify)
+		.bundle()
+		.on('error', console.error.bind(console))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest('./dist'));
 });
---
+++ src/front/index.js
@@ -0,0 +1,2 @@
+$ = jQuery = require('jquery');
+
