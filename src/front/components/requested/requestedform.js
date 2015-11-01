92bd2a046da551163d80e6a6060f2f43b9d20175
hai5nguy <hai5nguy@gmail.com>
2015-11-01 15:34:08 -0500

    got rid of routes

--- src/front/components/requested/requestedform.js
+++ src/front/components/requested/requestedform.js
@@ -3,6 +3,9 @@
 var React = require('react');
 
 module.exports = React.createClass({
+	// getInitialState: function () {
+	// 	return [];
+	// },
 	render: function () {
 		return (
 			<div>
--- src/front/index.js
+++ src/front/index.js
@@ -1,13 +1,26 @@
 $ = jQuery = require('jquery');
 
-require('bootstrap');
 
-var React 	= require('react');
-var Router 	= require('react-router');
-var routes 	= require('./routes');
 
 (function () {
 	'use strict';
+
+	require('bootstrap');
+
+	var React 	= require('react');
+	var Router 	= require('react-router');
+
+	var Route 			= Router.Route;
+	var DefaultRoute 	= Router.DefaultRoute;
+
+	var routes = (
+		<Route path="/" handler={require('./components/app')}>
+			<DefaultRoute handler={require('./components/home/homepage')} />
+			<Route path="requested" handler={require('./components/requested/requestedpage')} />
+			<Route path="requested-create" handler={require('./components/requested/requestedform')} />
+			<Route path="about" handler={require('./components/about/aboutpage')} />
+		</Route>
+	);
 
 	Router.run(routes, Router.HistoryLocation, function (Handler) {
 		React.render(<Handler />, document.getElementById('app'));
