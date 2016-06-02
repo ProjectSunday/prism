22e4a3393ffd437e7ffc55eeb4e97e8e3044c8b0
hai5nguy <hai5nguy@gmail.com>
2016-06-01 20:18:08 -0400

    lost frontend routing

--- server.dev.js
+++ server.dev.js
@@ -1,4 +1,5 @@
 import express         		from 'express'
+import path 				from 'path'
 import webpack 				from 'webpack'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
@@ -8,12 +9,14 @@ import config               from './webpack.config.dev'
 var app = express()
 var compiler = webpack(config)
 
-app.use(webpackDevMiddleware(compiler, {
+var devMiddleware = webpackDevMiddleware(compiler, {
 	// hot: true,
 	// historyApiFallback: { index: config.output.publicPath },
     publicPath: config.output.publicPath,
     stats: { chunks: false, colors: true }
-}))
+})
+
+app.use(devMiddleware)
 
 app.use(webpackHotMiddleware(compiler))
 
@@ -21,9 +24,24 @@ app.get('/authentication', function (req, res) {
 	res.send('Authenticated')
 })
 
-app.use('*', (req, res) => {
-	res.redirect('/index.html')
+
+//because html5 history is hard
+app.use((req, res, next) => {
+  const reqPath = req.url
+  const file = _.last(reqPath.split('/'))
+  if (['bundle.js', 'index.html'].indexOf(file) !== -1) {
+    res.end(devMiddleware.fileSystem.readFileSync(path.join(config.output.path, file)))
+  } else if (file.indexOf('.') === -1) {
+    res.end(devMiddleware.fileSystem.readFileSync(path.join(config.output.path, 'index.html')))
+  } else {
+    next()
+  }
 })
+
+
+// app.get('*', (req, res) => {
+// 	res.redirect('/index.html')
+// })
 
 
 var port = process.env.PORT || 7000
