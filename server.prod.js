a36c1e9cf03e8dbd1ef7776329cd4fab37a363ce
Hai Nguyen <hai5nguy@gmail.com>
2016-08-14 19:43:39 -0400

    [Sun Aug 14 19:43:39 EDT 2016]

--- server.prod.js
+++ server.prod.js
@@ -4,7 +4,7 @@ import webpack 				from 'webpack'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
 
-import config               from './webpack.config.dev'
+import config               from './webpack.config.prod'
 
 var app = express()
 var compiler = webpack(config)
